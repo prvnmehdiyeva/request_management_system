ae58fe395925ab6f941ef4bd00600844a59145e1
gerayvahidli <gerayvahidli@gmail.com>
2024-03-13 15:32:20 +0400

    fixes

--- src/app/main/home/request-detail/request-detail.component.ts
+++ src/app/main/home/request-detail/request-detail.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Inject, PLATFORM_ID } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { InquiriesInfo } from '../../../models/inquiries-info';
 import { AuthService } from '../../../services/auth.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AppComment } from '../../../models/appcomment';
 import { v4 as uuidv4 } from 'uuid';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-request-detail',
@@ -22,40 +23,49 @@ export class RequestDetailComponent implements OnInit {
   public inquiries: InquiriesInfo[] = [];
   public comments: AppComment[] = [];
   now:string=''
-  activeItem: string = 'commentButton'; 
+  activeItem: string = 'commentButton';
   newCommentTitle: string='';
   newCommentText: string='';
+  currentUser: any
 
-  constructor(private fb:FormBuilder, private route: ActivatedRoute, public authService: AuthService) {
+  constructor(private fb:FormBuilder, private route: ActivatedRoute, public authService: AuthService,@Inject(PLATFORM_ID) private platformId:Object) {
     this.form = this.fb.group({
       title: ['', Validators.required],
       text: ['', Validators.required]
     });
   }
-  
+
   ngOnInit(): void {
     this.route.paramMap.subscribe(params => {
       this.inquiryId = params.get('id') || '';
-      
-      const currentUserString = sessionStorage.getItem('currentUser');
-      if (currentUserString) {
-        const currentUser = JSON.parse(currentUserString);
 
-        this.id = currentUser.id || '';
-        this.userName = currentUser.name || '';
-        this.userJob = currentUser.job || '';
 
-        console.log('Inquiry ID:', this.inquiryId);
-        console.log('User ID:', this.id);
-        
-        this.getInquiries(); 
-        this.getComments();
-        this.nowDate();
-      } else {
-        console.error('currentUser not found in sessionStorage.');
+      if(isPlatformBrowser(this.platformId)){
+        const currentUserString = sessionStorage.getItem('currentUser');
+
+        if (currentUserString) {
+          const currentUser = JSON.parse(currentUserString);
+
+          this.id = currentUser.id || '';
+          this.userName = currentUser.name || '';
+          this.userJob = currentUser.job || '';
+
+          console.log('Inquiry ID:', this.inquiryId);
+          console.log('User ID:', this.id);
+
+          this.getInquiries();
+          this.getComments();
+          this.nowDate();
+        } else {
+          console.error('currentUser not found in sessionStorage.');
+        }
       }
-    });
-  } 
+
+
+      })
+
+
+  }
 
   nowDate(){
     const now = new Date();
@@ -71,7 +81,7 @@ export class RequestDetailComponent implements OnInit {
       this.inquiries = inquiries;
       this.filteredInquiries = inquiries.filter((inquiry: { ID: { toString: () => string; }; }) => inquiry.ID && inquiry.ID.toString() === this.inquiryId);
     });
-  } 
+  }
 
   getComments(){
     this.authService.getComments().subscribe(comments =>{
@@ -95,8 +105,8 @@ export class RequestDetailComponent implements OnInit {
 
       this.authService.addComment(newComment).subscribe(
         (comment: AppComment) => {
-          this.comments.unshift(comment); 
-          this.form.reset(); 
+          this.comments.unshift(comment);
+          this.form.reset();
         },
         (error) => {
           console.error('Error adding comment:', error);
