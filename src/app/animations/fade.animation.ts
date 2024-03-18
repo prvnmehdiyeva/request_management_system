cd162ce59be9d99b03bc71beaa2bb5a7b5f6db05
prvnmehdiyeva <parvinrm@code.edu.az>
2024-03-18 13:09:52 +0400

    burger menu and table

---
+++ src/app/animations/fade.animation.ts
@@ -0,0 +1,27 @@
+import { trigger, transition, style, animate } from '@angular/animations';
+
+export const fadeInAnimation = trigger('fadeInAnimation', [
+  transition(':enter', [
+    style({ 
+      opacity: 0,
+      transform: 'translateY(-10%)', 
+    }),
+    animate('0.5s 0.5s ease-out', style({ 
+      opacity: 1,
+      transform: 'translateY(0)', 
+    })),
+  ]),
+]);
+
+export const fadeOpacityAnimation = trigger('fadeOpacityAnimation', [
+  transition(':enter', [
+    style({ 
+      opacity: 0,
+      transform: 'translateX(-5%)', 
+    }),
+    animate('0.5s 0.5s ease-in', style({ 
+      opacity: 1,
+      transform: 'translateX(0)', 
+    })),
+  ]),
+]);
